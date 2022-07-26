b5c65c3503de7daa4d2b3d794ec6d4591316b728
shalom69 <shalomaevanton@gmail.com>
2022-07-26 16:16:47 +0300

    java bonus

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2000;
+        int bonus = amount / 20;
+
+        System.out.println(bonus + " миль");
+
+
+    }
+}
